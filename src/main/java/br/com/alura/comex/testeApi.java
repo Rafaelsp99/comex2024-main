053d58646d46d845eac47cbf07f94756ec612482
Rafaelsp99 <79008827+Rafaelsp99@users.noreply.github.com>
2024-02-09 16:12:25 -0300

    6 - Fazer consulta para a API
    6 - Fazer consulta para a API

---
+++ src/main/java/br/com/alura/comex/testeApi.java
@@ -0,0 +1,35 @@
+package br.com.alura.comex;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+public class testeApi{
+    public static void main (String[] args) throws IOException, InterruptedException {
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+            String endereco = "https://fakestoreapi.com/products/1" ;
+
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(endereco))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+                String json = response.body();
+                System.out.println(json);
+
+        System.out.println("endereco");
+        }
+
+}
